aaa59dc354d735b7b2ea1a3996b97fc77d2ae6a4
Andrew Meredith <andrew@lambdasoftware.com>
2018-03-17 22:44:01 -0600

    Resolve race condition between registering webhook endpoint and initiating handshake

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Connector for [conveyor-client](https://github.com/kendru/conveyor-client)
 to enable WebHook subscriptions using the koa web framework.
 
+This connector sets up a randomly generated endpoint and performs a
+challenge-response handshake with the Conveyor server once Koa has
+started up an HTTP server and is ready to accept connections.
+
 ### Usage
 
 ```
@@ -11,15 +15,15 @@ const bodyParser = require('koa-bodyparser');
 const conveyor = require('conveyor-client');
 const connect = require('conveyor-koa-connector');
 
-const app = new Koa();
-const c = conveyor.Client('localhost', 3000, false);
-app.use(bodyParser());
+(async function initialize() {
+    const app = new Koa();
+    const c = conveyor.Client('localhost', 3000, false);
+    app.use(bodyParser());
 
-app.listen(8080, async () => {
-    // The application should not be connected until after it is ready to
-    // receive connections, since conveyor will immediately issue a WebHook
-    // challenge before accepting subscriptions.
     await connect(app, c.getConnection(), 'http://localhost:8080');
-    console.log('Connected to Conveyor for subscriptions.');
-});
+
+    app.listen(8080, () => {
+        console.log('Connected to Conveyor for subscriptions.');
+    });
+})();
 ```
--- src/index.js
+++ src/index.js
@@ -1,34 +1,70 @@
 const { impl } = require('conveyor-client')
 const crypto = require('crypto')
 
+class EventBuffer {
+    
+    constructor() {
+        this.events = []
+        this.emitImpl = null
+    }
+
+    emit(event) {
+        if (this.emitImpl) {
+            this.emitImpl(event)
+        } else {
+            this.events.push(event)
+        }
+    }
+
+    setEmitImpl(emitImpl) {
+        this.emitImpl = this.emitImpl
+        if (this.events.length > 0) {
+            this.events.forEach(e => this.emitImpl(e))
+            this.events = []
+        }
+    }
+}
+
 async function connect(koaApp, connectionOrClient, baseUrl) {
     const callbackEndpoint = `/${crypto.randomBytes(16).toString('hex')}`
+    
+    const buffer = new EventBuffer();
+    koaApp.use(async function withConveyorSubscriptionWebhook(ctx, next) {
+        if (ctx.path !== callbackEndpoint) {
+            await next()
+            return
+        }
+        
+        if (ctx.method === 'GET') {
+            const { challenge } = ctx.query
+            ctx.body = challenge
+            return
+        } else if (ctx.method === 'POST' && typeof ctx.request.body === 'object') {
+            buffer.emit(ctx.request.body)
+            ctx.status = 200
+        }
+    })
+
     let connection
     if (connectionOrClient.getConnection) {
         connection = connectionOrClient.getConnection()
     } else {
         connection = connectionOrClient
     }
-
-    await impl.registerSubscriptionImpl(connection, function registerImplementation(emit, cancel) {
-        koaApp.use(async function (ctx, next) {
-            if (ctx.path !== callbackEndpoint) {
-                await next()
-                return
-            }
-            
-            if (ctx.method === 'GET') {
-                const { challenge } = ctx.query
-                ctx.body = challenge
-                return
-            } else if (ctx.method === 'POST' && typeof ctx.request.body === 'object') {
-                emit(ctx.request.body)
-                ctx.status = 200
-            }
+    
+    // Defer registration until the koa app is up and ready to respond to the webhook
+    const oldCallback = koaApp.callback.bind(koaApp)
+    koaApp.callback = function conveyorWrappedCallback() {
+        impl.registerSubscriptionImpl(connection, function registerImplementation(emit, cancel) {
+            buffer.setEmitImpl(emit)
+            // TODO: add hook to call cancel on shutdown - may be able to hook into Koa,
+            // otherwise we would need to catch process signals
+            return baseUrl + callbackEndpoint
         })
-
-        return baseUrl + callbackEndpoint
-    })
+        .catch((err) => console.error('Error subscribing to Conveyor events', err))
+        
+        return oldCallback()
+    }
 }
 
 module.exports = connect
